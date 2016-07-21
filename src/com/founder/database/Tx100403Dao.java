dc21e89b635c5da64090302567dd59d695e27f2b
hao0oah <739124722@qq.com>
2016-07-21 11:35:42 +0800

    Tx100403Dao 增加个人/对公区分，方法名称确认

--- src/com/founder/database/Tx100403Dao.java
+++ src/com/founder/database/Tx100403Dao.java
@@ -11,21 +11,27 @@ public interface Tx100403Dao {
 	 * 获取频繁开户可疑客户信息<br>
 	 * 条件：同一身份证件在5个工作日内累计开户大于等于3张卡
 	 */
-	Tx100403 getF1001Accnt(Map<String,?> param);
+	Tx100403 getF1001grCust(Map<String,?> param);
+	Tx100403 getF1001dgCust(Map<String,?> param);
+	
 	/**
-	 * 获取频繁开户可疑客户卡号信息
+	 * 获取频繁开户可疑客户账户信息
 	 */
-	List<TxUnusualOpencard_Accounts> getF1001List(Map<String,?> param);
+	List<TxUnusualOpencard_Accounts> getF1001grAcntList(Map<String,?> param);
+	List<TxUnusualOpencard_Accounts> getF1001dgAcntList(Map<String,?> param);
 	
 	
 	/**
 	 * 获取累计开户可以客户信息<br>
 	 * 条件：同一身份证件在同一法人银行累计开户大于等于10张卡
 	 */
-	Tx100403 getF1002Accnt(Map<String,?> param);
+	Tx100403 getF1002grCust(Map<String,?> param);
+	Tx100403 getF1002dgCust(Map<String,?> param);
+	
 	/**
-	 * 获取累计开户可以客户卡号信息
+	 * 获取累计开户可以客户账户信息
 	 */
-	List<TxUnusualOpencard_Accounts> getF1002List(Map<String,?> param);
+	List<TxUnusualOpencard_Accounts> getF1002grAcntList(Map<String,?> param);
+	List<TxUnusualOpencard_Accounts> getF1002dgAcntList(Map<String,?> param);
 	
 }
